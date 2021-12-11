8943ee6b04dbcea91340bf20dccd4666a2578db2
Ashfaaq Rifath <ahamedashfaaq4@gmail.com>
2021-12-11 15:34:42 +0530

    Update password_generator.py
    changing the entire code and adding comments.

--- password_generator.py
+++ password_generator.py
@@ -1,27 +1,58 @@
 import secrets
 import string
 
+import os.path
+
 import colorama
 from colorama import Fore, Back
 colorama.init(autoreset=True)
 
+print("Copyright(c) 2022 Ashfaaq Rifath")
 print(Fore.BLACK + Back.YELLOW + " PASSWORD MANAGER ")
 
-add_new = input(Fore.YELLOW + "Add new or generate password (a/g): ")
+#asks the user if they need to generate or add a new password
+pass_option = input("Add new or generate password (a/g): ")
 
-if add_new.lower() == "a":
-    pass_name = input(Fore.YELLOW + "Name your password: ")
-    my_pass = input(Fore.YELLOW + "Input your password: ")
-    add_pass = open(f"Password for {pass_name}.txt", "w")
-    add_pass.write(f"Saved password for {pass_name}: ")
-    add_pass.write(my_pass)
-    add_pass.close()
-    print(Fore.GREEN + "Password saved")
+if pass_option.lower() == "a":
+    #the desired file path should be added here (hardcoded)
+    default_path = "file path goes here"
+    pass_name = input("Name your password: ")
+    save_path = os.path.join(default_path, pass_name + ".txt")
+    my_pass = input("Input your password: ")
+    user_pass = len(my_pass)
+    if user_pass > 14:
+        #also contains a password strength meter
+        print(Fore.BLACK + Back.GREEN + " Your password is strong ")
 
-elif add_new.lower() == "g":
-    name = input(Fore.BLUE + "Name your password: ")
+    elif user_pass < 14:
+        print(Fore.BLACK + Back.RED + " Password should be stronger ")
+        #takes the file path the user wants as an input
+    choose_loc = input("Save to default location or custom location (d/c): ")
+
+    if choose_loc.lower() == "d":
+        add_pass = open(save_path, "w")
+        add_pass.write(f"Saved password for {pass_name}: ")
+        add_pass.write(my_pass)
+        add_pass.close()
+        print(Fore.GREEN + "Password saved")
+
+    elif choose_loc == "c":
+        print(Fore.BLACK + Back.YELLOW + " Enter correct file path ")
+        custom_path = input("Enter file path: ")
+        save_abs_path = os.path.join(custom_path, pass_name + ".txt")
+        add_pass = open(save_abs_path, "w")
+        add_pass.write(f"Saved password for {pass_name}: ")
+        add_pass.write(my_pass)
+        add_pass.close()
+        print(Fore.GREEN + "Password saved")
+
+    else:
+        print(Fore.BLACK + Back.RED + "INVALID OPTION")
+
+elif pass_option.lower() == "g":
+    name = input("Name your password: ")
     try:
-        pass_length = int(input(Fore.BLUE + "Enter your password length: "))
+        pass_length = int(input("Enter password length: "))
     except:
         print(Fore.BLACK + Back.RED + " ENTER VALID NUMBER ")
     special_chars = "!@#$%^&*_~;?<>{}[]"
@@ -37,19 +68,41 @@ elif add_new.lower() == "g":
             strength = len(password)
             if strength > 14:
                 print(Fore.BLACK + Back.GREEN + " Your password is strong ")
+
             elif strength < 14:
                 print(Fore.BLACK + Back.RED + " Password should be stronger ")
-            need_save = input(Fore.BLUE + "Do you want to save this password? (y/n): ")
+
+            need_save = input("Do you want to save this password? (y/n): ")
+
+            if need_save.lower() == "y":
+                choose_loc = input("Save to default location or custom location (d/c): ")
+
+            elif need_save == "n":
+                print("---------------")
+
+            else:
+                print(Fore.BLACK + Back.RED + " INVALID OPTION ")
             break
 
-    if need_save.lower() == "y":
-        saving = open(f"Password for {name}.txt","w")
+    if choose_loc.lower() == "d":
+        save_path = "file path goes here"
+        def_path = os.path.join(save_path, name + ".txt")
+        saving = open(def_path,"w")
         saving.write(f"Generated password for {name}: ")
         saving.write(password)
         saving.close()
         print(Fore.GREEN + "Password saved")
-    elif need_save == "n":
-        print("---------------")
+
+    elif choose_loc.lower() == "c":
+        print(Fore.BLACK + Back.YELLOW + " Enter correct file path ")
+        save_path = input("Enter file path: ")
+        cus_path = os.path.join(save_path, name + ".txt")
+        gen_pass = open(cus_path, "w")
+        gen_pass.write(f"Saved password for {name}: ")
+        gen_pass.write(password)
+        gen_pass.close()
+        print(Fore.GREEN + "Password saved")
+
     else:
         print(Fore.BLACK + Back.RED + " INVALID OPTION ")
 
